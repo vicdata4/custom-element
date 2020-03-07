d68d586eb076fa8919e5c726c6e28462c226e0b0
vicdata <vicdata@outlook.com>
2020-03-07 19:28:48 +0100

    get label() example + README.md file

--- components/my-button.js
+++ components/my-button.js
@@ -38,15 +38,18 @@ class Button extends HTMLElement {
     this._shadowRoot.appendChild(template.content.cloneNode(true));
     this.$button = this._shadowRoot.querySelector('button');
   }
+  get label() {
+    return this.getAttribute('label');
+  }
   static get observedAttributes() {
     return ['label'];
   }
   attributeChangedCallback(name, oldVal, newVal) {
-    this[name] = newVal;
     this.render();
   }
   render() {
     this.$button.innerHTML = this.label;
   }
 }
+
 window.customElements.define('my-button', Button);
